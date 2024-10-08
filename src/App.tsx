45c58dbc46477ba40c8b4f866834b3b8a0293916
suite <gr8bitmail@gmail.com>
2024-10-07 23:52:18 -0600

    great, working

--- src/App.tsx
+++ src/App.tsx
@@ -141,7 +141,7 @@ function App() {
             </nav>
 
             {/* Horizontal line container */}
-            <div className="relative w-screen left-1/2 right-1/2 -mx-[50vw]">
+            <div className="w-full">
               <hr className="border-t border-[#E4E4E4]" />
             </div>
     
@@ -155,7 +155,7 @@ function App() {
             <div className="mb-4 flex justify-center">
               <div 
                 style={{ paddingBottom: `${(1 / imageAspectRatio) * 100}%` }}
-                className="w-full max-w-[400px] relative"
+                className="w-full relative"
               >
                 {showLoader && (
                   <div className="absolute inset-0 flex items-center justify-center">
@@ -177,26 +177,27 @@ function App() {
             </p>
 
             {/* View on X button */}
-            <div className="mb-8">
-              <a href={currentItem["Original post (link)"]} target="_blank" rel="noopener noreferrer" className="bg-white text-[#2600FF] border border-[#2600FF] rounded-full px-4 py-2 text-base inline-flex items-center">
-                View on <svg width="16" height="16" viewBox="0 0 300 300.251" version="1.1" xmlns="http://www.w3.org/2000/svg" className="ml-2">
-                  <path fill="#2600FF" d="M178.57 127.15 290.27 0h-26.46l-97.03 110.38L89.34 0H0l117.13 166.93L0 300.25h26.46l102.4-116.59 81.8 116.59h89.34M36.01 19.54H76.66l187.13 262.13h-40.66"/>
+            <div className="mb-6">
+              <a href={currentItem["Original post (link)"]} target="_blank" rel="noopener noreferrer" className="bg-white text-[#2600FF] border border-[#2600FF] hover:text-[#170099] hover:border-[#170099] rounded-full px-4 py-1 text-base inline-flex items-center transition-colors duration-200">
+                View on <svg width="16" height="16" viewBox="0 0 300 300.251" version="1.1" xmlns="http://www.w3.org/2000/svg" className="ml-2 transition-colors duration-200">
+                  <path fill="currentColor" d="M178.57 127.15 290.27 0h-26.46l-97.03 110.38L89.34 0H0l117.13 166.93L0 300.25h26.46l102.4-116.59 81.8 116.59h89.34M36.01 19.54H76.66l187.13 262.13h-40.66"/>
                 </svg>
               </a>
             </div>
           </div>
 
-          {/* Footer section */}
-          <div className="mt-auto pb-4 max-w-[400px] mx-auto w-full">
+          <div className="mt-auto md:mt-8 pb-4 md:pb-0">
             {/* Site hits */}
-            <div className="flex justify-center mb-2">
-              <div className="bg-[#E4E4E4] text-[#696969] text-sm px-4 py-2 inline-block">
-                Site hits: <span className="font-bold">{hitCount.toLocaleString()}</span>
+            <div className="flex justify-center mb-2 max-w-[400px] mx-auto w-full px-4">
+              <div className="text-[#A8A8A8] text-[14px] text-center">
+                Site smashed <span className="font-bold">{hitCount.toLocaleString()}</span> times.
               </div>
             </div>
             {/* Created by */}
-            <footer className="text-[#A8A8A8] text-sm text-center p-2">
-              Created by <u><a href="https://twitter.com/capsjpeg" target="_blank" rel="noopener noreferrer">Caps</a></u>, <u><a href="https://twitter.com/misterholana" target="_blank" rel="noopener noreferrer">h_</a></u>, and <u><a href="https://twitter.com/0x_saddy" target="_blank" rel="noopener noreferrer">Saddy</a></u>.
+            <footer className="text-[#A8A8A8] text-[14px] text-center p-2 md:py-3 md:bg-[#F5F5F5] md:w-full mt-2 md:mt-0">
+              <div className="max-w-[400px] mx-auto w-full px-4">
+                Created by <u><a href="https://twitter.com/capsjpeg" target="_blank" rel="noopener noreferrer" className="hover:text-black">Caps</a></u>, <u><a href="https://twitter.com/misterholana" target="_blank" rel="noopener noreferrer" className="hover:text-black">h_</a></u>, and <u><a href="https://twitter.com/0x_saddy" target="_blank" rel="noopener noreferrer" className="hover:text-black">Saddy</a></u>.
+              </div>
             </footer>
           </div>
         </>
